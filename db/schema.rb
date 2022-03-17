3b38106b7e74294a6a0b2edf57b485e9b88e0472
Ajayi Peter <peterdgreat2016@gmail.com>
2022-03-17 21:50:57 +0100

    added name to admin permit

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_17_194915) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
