d2e43fe19ae8a7b348d80638999cd806f8d9ca77
Ajayi Peter <peterdgreat2016@gmail.com>
2022-03-17 21:32:02 +0100

    setup devise for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # devise for admin api v1 routes
+  scope :api, defaults: { format: :json } do
+  scope :v1 do
+    devise_for :admins, controllers: {
+      sessions: 'admins/sessions',
+      registrations: 'admins/registrations'
+    }
+  end
+end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
