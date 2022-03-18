1e257e6d1fab3fc5c4ff6311cc922e3e673e63bf
Ajayi Peter <peterdgreat2016@gmail.com>
2022-03-18 22:36:58 +0100

    setup posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,21 @@
 class Api::V1::PostsController < ApplicationController
   def index
+    @posts = Post.all
+    render json: @posts
   end
 
-  def show
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:category, :image)
   end
 end
