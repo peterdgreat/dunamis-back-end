d1dd9b9982fee8d19a27bb06d605c44982317114
Ajayi Peter <peterdgreat2016@gmail.com>
2022-04-23 00:18:11 +0200

    handled current_admin temporary

--- app/controllers/api/v1/admins/sessions_controller.rb
+++ app/controllers/api/v1/admins/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Admins::SessionsController < Devise::SessionsController
+  respond_to :json
   before_action :sign_in_params, only: :create
   before_action :load_user, only: :create
   # sign in
@@ -11,14 +12,24 @@ class Api::V1::Admins::SessionsController < Devise::SessionsController
         data: {}
 
       }, status: :unauthorized
+    # elsif @user.valid_password?(sign_in_params[:password])
+    #   sign_in 'user', @user
+    #   render json: {
+    #     messages: 'Signed In Successfully',
+    #     is_success: true,
+    #     data: { user: @user }
+    #   }, status: :ok
 
     elsif @user.valid_password?(sign_in_params[:password])
-      sign_in 'user', @user
+      resource = warden.authenticate(auth_options)
+      sign_in(@user, resource)
       render json: {
-        messages: 'Signed In Successfully',
+        messages: 'Sign in successfully',
         is_success: true,
         data: { user: @user }
       }, status: :ok
+
+      # puts current_admin
     else
       render json: {
         messages: 'Invalid password',
@@ -28,9 +39,25 @@ class Api::V1::Admins::SessionsController < Devise::SessionsController
     end
   end
 
+  def current_admin
+    @current_admin = Admin.first
+    render json: @current_admin
+  end
+
+  def destroy
+    sign_out(@user)
+    render json: {
+      messages: 'Signed Out Successfully',
+      is_success: true,
+      data: {}
+
+    }, status: :ok
+  end
+
   private
 
   def sign_in_params
+    # permit as json
     params.permit(:email, :password)
   end
 
@@ -38,13 +65,15 @@ class Api::V1::Admins::SessionsController < Devise::SessionsController
     @user = Admin.find_for_database_authentication(email: sign_in_params[:email])
   end
 
-  # DELETE /resource/sign_out
-  def destroy
-    sign_out(:admin)
-    render json: {
-      message: 'Logged out'
-    }
+  def auth_options
+    { scope: resource_name, recall: "#{controller_path}#failure" }
   end
+
+  def to_json(resource)
+    resource.as_json(only: %i[email created_at updated_at])
+  end
+
+  # DELETE /resource/sign_out
 
   # protected
 
