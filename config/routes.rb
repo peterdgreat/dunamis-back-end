cf1bd4a804daaa48610a0134dd09ec2b31c3e2b2
Ajayi Peter <peterdgreat2016@gmail.com>
2022-04-26 00:07:12 +0200

    removed unused codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # root to: "application#cookie"
   devise_for :admins
   namespace :api , defaults: {format: :json}  do
     namespace :v1 do
@@ -7,8 +6,6 @@ Rails.application.routes.draw do
       get 'posts/show'
       post 'posts/create'
       delete 'posts/:id/destroy', to: 'posts#destroy'
-    #  get 'admin',to: 'admins/lists#current_user'
-
       devise_scope :admin do
         post 'admins/sign_in', to: 'admins/sessions#create'
  get  'admin',to: 'admins/sessions#current_admin'
