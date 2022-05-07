b9446e7223aa42e1773b3d36c71b21aeb87e95d5
Ajayi Peter <peterdgreat2016@gmail.com>
2022-05-07 16:50:05 +0200

    origin remote cors url added

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-  origins '*' 
+  origins 'https://dunamis-backend.herokuapp.com/' 
 
 
     resource "*",
